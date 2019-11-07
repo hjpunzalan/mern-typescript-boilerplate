f636a3dd032b62a991144225392e88a327862512
hjpunzalan <hj.punzalan@hotmail.com>
2019-11-07 22:26:47 +0800

    initial app, started with app.ts and server.ts

---
+++ app.ts
@@ -0,0 +1,41 @@
+import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import mongoSanitize from 'express-mongo-sanitize';
+import xss from 'xss-clean';
+import hpp from 'hpp';
+
+export const app = express();
+
+//////////////////////// Global Middlewares//////////////////////////
+// Add htp headers that secure the server
+app.use(helmet());
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+}
+
+// Converts incoming json data to js object ---- Body parser that reads data from body into req.body
+app.use(express.json({ limit: '10kb' })); // package will parse 10kb into meaningful data
+
+// Cookie parser
+app.use(cookieParser());
+
+// Data sanitization against NoSQL query injection
+//Look at the req and filter out all '$' and '.' that sends queries to db illegaly
+app.use(mongoSanitize());
+
+// Data sanitization again XSS ( cross site scripting attacks)
+// Sanitizes incoming http request from req.body
+app.use(xss()); // Converts malicious html code into dull code
+
+// Prevent parameter pollution
+// prevents adding duplicated parameters in query
+// The whitelist works for both req.query and req.body.
+app.use(
+	hpp({
+		whitelist: [] // add http parameters used
+	})
+);
---
+++ server.ts
@@ -0,0 +1,46 @@
+import { app } from './app';
+import dotenv from 'dotenv';
+// import mongoose from 'mongoose';
+
+// SYNC Unhandled rejections
+// listening to event uncaughtException
+process.on('uncaughtException', err => {
+	console.log('UNCAUGHT Exception! Shutting down...');
+	console.log(err.name, err.message);
+	process.exit(1); // 0 success , 1 for unhandled rejection
+});
+
+// Must be your own private process variables
+dotenv.config({ path: './config.env' });
+
+// Connecting to mongoDB using mongoose
+// const DB = process.env.DATABASE;
+// mongoose
+// 	.connect(DB, {
+// 		useNewUrlParser: true,
+// 		useCreateIndex: true,
+// 		useFindAndModify: false
+// 	})
+// 	.then(() => console.log('DB connection is successful'));
+
+// Development / Production mode
+console.log(`Server running on: ${process.env.NODE_ENV} mode`);
+
+const port = process.env.PORT || 8000;
+const server = app.listen(port, () => {
+	console.log(`App running on port ${port}`);
+});
+
+// ASYNC Promises
+// process object will emmit unhandled rejection
+// promise rejection to have last safety nets
+process.on('unhandledRejection', (err: Error) => {
+	console.log('UNHANDLED REJECTION! Shutting down...');
+	console.log(err.name, err.message);
+	server.close(() => {
+		//  BY having server.close finishes all request that is being handled then closes the app
+		process.exit(1); // 0 success , 1 for unhandled rejection
+	});
+});
+
+// Need a tool that restarts application
