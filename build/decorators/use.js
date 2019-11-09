62fb558a5738b34c75fcc2d96347a035f8118370
hjpunzalan <hj.punzalan@hotmail.com>
2019-11-09 22:23:42 +0800

    refined middlewares to use only one decorator

--- build/decorators/use.js
+++ build/decorators/use.js
@@ -3,11 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
 var enums_1 = require("./enums");
 // middlewares are requesthandlers
-function use(middleware) {
+function use() {
+    var middlewares = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        middlewares[_i] = arguments[_i];
+    }
     return function (target, key, desc) {
-        // Get the middleware or initiate an array of middlewares
-        var middlewares = Reflect.getMetadata(enums_1.MetadataKeys.Middleware, target, key) || [];
-        Reflect.defineMetadata(enums_1.MetadataKeys.Middleware, middlewares.concat([middleware]), target, key);
+        Reflect.defineMetadata(enums_1.MetadataKeys.Middleware, middlewares, target, key);
     };
 }
 exports.use = use;
--- decorators/use.ts
+++ decorators/use.ts
@@ -3,17 +3,8 @@ import { MetadataKeys } from "./enums";
 import { RequestHandler } from "express";
 
 // middlewares are requesthandlers
-export function use(middleware: RequestHandler) {
+export function use(...middlewares: RequestHandler[]) {
 	return function(target: any, key: string, desc: PropertyDescriptor) {
-		// Get the middleware or initiate an array of middlewares
-		const middlewares =
-			Reflect.getMetadata(MetadataKeys.Middleware, target, key) || [];
-
-		Reflect.defineMetadata(
-			MetadataKeys.Middleware,
-			[...middlewares, middleware],
-			target,
-			key
-		);
+		Reflect.defineMetadata(MetadataKeys.Middleware, middlewares, target, key);
 	};
 }
