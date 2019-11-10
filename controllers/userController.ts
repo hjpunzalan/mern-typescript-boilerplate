bf46d328df66f27a855e21bd4bef1e1aa36a9d11
hjpunzalan <hj.punzalan@hotmail.com>
2019-11-10 21:58:40 +0800

    added getAllusers and queryHandling util

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -2,16 +2,17 @@ import { Request, Response, NextFunction, Router } from "express";
 import { controller, use, catchAsync, post, get } from "../decorators";
 import { Users } from "../models/Users";
 import { bodyValidator } from "../middlewares/bodyValidator";
+import { QueryHandling } from "./../utils/queryHandling";
 
 export const userRoute = Router();
 
 // commented out password in user model
-@controller("/user", userRoute)
+@controller("/users", userRoute)
 class UserController {
 	@post("/register")
 	@use(bodyValidator("firstName", "lastName", "email"))
 	@catchAsync
-	public async registerUser(req: Request, res: Response, next: NextFunction) {
+	async registerUser(req: Request, res: Response, next: NextFunction) {
 		const { firstName, lastName, email } = req.body;
 		const newUser = await Users.create({ firstName, lastName, email });
 
@@ -21,5 +22,14 @@ class UserController {
 		// need to send an email with default password of user
 		// password must only be seen by the user and not the admin that registered user
 		res.status(201).json(newUser);
+	}
+
+	@get("/")
+	@catchAsync
+	async getAllUsers(req: Request, res: Response, next: NextFunction) {
+		const query = Users.find();
+		const features = new QueryHandling(query, req.query).sort().filter();
+		const users = await features.query;
+		res.status(200).json(users);
 	}
 }
