d537e6f2f0aace89d6e90ef1b60fa69cfe6d5556
hjpunzalan <hj.punzalan@hotmail.com>
2019-11-10 21:07:52 +0800

    adding build folder to gitignore

--- build/decorators/enums/MetadataKeys.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var MetadataKeys;
-(function (MetadataKeys) {
-    MetadataKeys["Method"] = "method";
-    MetadataKeys["Path"] = "path";
-    MetadataKeys["Middleware"] = "middleware";
-    MetadataKeys["Validator"] = "validator";
-})(MetadataKeys = exports.MetadataKeys || (exports.MetadataKeys = {}));
