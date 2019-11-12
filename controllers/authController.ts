def9a2f593ed491bdb59242bf4ff4fe7bce9acf5
hjpunzalan <hj.punzalan@hotmail.com>
2019-11-12 18:16:36 +0800

    added logging out and user info within session

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -4,42 +4,56 @@ import { Users } from "../models/Users";
 import { AppError } from "./../utils/appError";
 import { bodyValidator } from "../middlewares/bodyValidator";
 import { QueryHandling } from "./../utils/queryHandling";
+import { requireAuth } from "../middlewares/requireAuth";
 
 export const authRoute = Router();
 
 // commented out password in user model
 @controller("/auth", authRoute)
 class UserController {
-	@post("/login")
-	@use(bodyValidator("email", "password"))
-	@catchAsync
-	async login(req: Request, res: Response, next: NextFunction) {
-		const { email, password } = req.body;
+  @post("/login")
+  @use(bodyValidator("email", "password"))
+  @catchAsync
+  async login(req: Request, res: Response, next: NextFunction) {
+    const { email, password } = req.body;
 
-		// Password by default is not selected
-		const user = await Users.findOne({ email }).select("+password");
+    // Password by default is not selected
+    const user = await Users.findOne({ email }).select("+password");
 
-		// Verify user exist and password is correct
-		if (
-			!user ||
-			(user.password && !(await user.checkPassword(password, user.password)))
-		)
-			return next(
-				new AppError("Invalid email or password. Please try again.", 401)
-			);
+    // Verify user exist and password is correct
+    if (
+      !user ||
+      (user.password && !(await user.checkPassword(password, user.password)))
+    )
+      return next(
+        new AppError("Invalid email or password. Please try again.", 401)
+      );
 
-		if (!user.active) {
-			// If user's account has been deactivated, reactivate it.
-			user.active = true;
-			await user.save();
-		}
+    if (!user.active) {
+      // If user's account has been deactivated, reactivate it.
+      user.active = true;
+      await user.save();
+    }
 
-		// remove users password from response
-		user.password = undefined;
+    // remove users password from response
+    user.password = undefined;
 
-		// Add to session
-		if (req.session) req.session.loggedIn = true;
+    // Add to session
+    if (req.session) {
+      req.session.loggedIn = true;
+      req.session.user = user;
+    }
 
-		res.status(200).json(user);
-	}
+    res.status(200).json(user);
+  }
+
+  @get("/logout")
+  async logout(req: Request, res: Response, next: NextFunction) {
+    if (req.session)
+      req.session.destroy((err: Error) => {
+        if (err) return next(err);
+        res.clearCookie("sid");
+        res.status(200).send("User logged out");
+      });
+  }
 }
--- middlewares/requireAuth.ts
+++ middlewares/requireAuth.ts
@@ -2,13 +2,15 @@ import { NextFunction, Request, Response } from "express";
 import { AppError } from "../utils/appError";
 
 export function requireAuth(
-	req: Request,
-	res: Response,
-	next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ): void {
-	if (req.session && req.session.loggedIn) {
-		return next();
-	}
+  if (req.session && req.session.loggedIn) {
+    return next();
+  }
 
-	return next(new AppError("You are not logged in!", 403));
+  return next(
+    new AppError("You are not logged in! Please log in to gain access.", 403)
+  );
 }
