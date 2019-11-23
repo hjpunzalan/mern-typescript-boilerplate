f93cae6e444793e2a7157a507b1923fee43b4fc5
hjpunzalan <hj.punzalan@hotmail.com>
2019-11-23 17:41:45 +0800

    removed photo from User Model

--- server/models/Users.ts
+++ server/models/Users.ts
@@ -7,110 +7,109 @@ import { IUser } from "../interfaces/User";
 // use save to run validators again because find and update wont
 
 const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: [true, "User must have a first name"]
-  },
-  lastName: {
-    type: String,
-    required: [true, "User must have a last name"]
-  },
-  email: {
-    type: String,
-    required: [true, "User must have an email"],
-    unique: true,
-    lowercase: true, //not a validator but transforms string
-    validator: [validator.isEmail, "Must be a valid email address"]
-  },
-  photo: String,
-  password: {
-    type: String,
-    select: false, //Keeps password hidden from anywhere
-    minlength: 6,
-    required: [true, "User must have a password"]
-    // default: generator.generate({
-    // 	length: 6,
-    // 	numbers: true
-    // })
-  },
-  active: {
-    type: Boolean,
-    default: true
-  },
-  role: {
-    type: String,
-    enum: ["admin", "user"],
-    default: "user"
-  },
-  lastEdited: Date,
-  passwordChangedAt: {
-    type: Date,
-    select: false
-  },
-  passwordResetToken: String,
-  passwordResetExpires: Date
+	firstName: {
+		type: String,
+		required: [true, "User must have a first name"]
+	},
+	lastName: {
+		type: String,
+		required: [true, "User must have a last name"]
+	},
+	email: {
+		type: String,
+		required: [true, "User must have an email"],
+		unique: true,
+		lowercase: true, //not a validator but transforms string
+		validator: [validator.isEmail, "Must be a valid email address"]
+	},
+	password: {
+		type: String,
+		select: false, //Keeps password hidden from anywhere
+		minlength: 6,
+		required: [true, "User must have a password"]
+		// default: generator.generate({
+		// 	length: 6,
+		// 	numbers: true
+		// })
+	},
+	active: {
+		type: Boolean,
+		default: true
+	},
+	role: {
+		type: String,
+		enum: ["admin", "user"],
+		default: "user"
+	},
+	lastEdited: Date,
+	passwordChangedAt: {
+		type: Date,
+		select: false
+	},
+	passwordResetToken: String,
+	passwordResetExpires: Date
 });
 
 // Middleware
 
 userSchema.pre<IUser>(/^find/, function(next) {
-  // 'this' points to the current query before executing the event /^find/
-  if (this instanceof mongoose.Query)
-    this.find({ active: { $ne: false } }).select("-__v");
-  next();
+	// 'this' points to the current query before executing the event /^find/
+	if (this instanceof mongoose.Query)
+		this.find({ active: { $ne: false } }).select("-__v");
+	next();
 });
 // update changedPasswordAt when resetting password
 // Skips if password is NOT modified or NEW.
 userSchema.pre<IUser>("save", function(next) {
-  if (!this.isModified("password") || this.isNew) return next();
-  this.passwordChangedAt = Date.now() - 1000; // subtracting 1 second takes into account delay in saving into database so that its before the token is generated
-  next();
+	if (!this.isModified("password") || this.isNew) return next();
+	this.passwordChangedAt = Date.now() - 1000; // subtracting 1 second takes into account delay in saving into database so that its before the token is generated
+	next();
 });
 
 userSchema.pre<IUser>("save", async function(next) {
-  // Only run this function if password was actually modified
-  // Changing password or password creation eg. new password
-  if (!this.isModified("password")) return next();
-  const saltRounds = 12;
-  if (this.password)
-    this.password = await bcrypt.hash(this.password, saltRounds);
+	// Only run this function if password was actually modified
+	// Changing password or password creation eg. new password
+	if (!this.isModified("password")) return next();
+	const saltRounds = 12;
+	if (this.password)
+		this.password = await bcrypt.hash(this.password, saltRounds);
 
-  next();
+	next();
 });
 
 userSchema.methods.checkPassword = async function(
-  password: string,
-  hashedPassword: string
+	password: string,
+	hashedPassword: string
 ): Promise<boolean> {
-  return await bcrypt.compare(password, hashedPassword);
+	return await bcrypt.compare(password, hashedPassword);
 };
 
 // For changing password -- to keep track of
 userSchema.methods.changedPasswordAfter = function(
-  this: IUser,
-  timestamp: Date | number
+	this: IUser,
+	timestamp: Date | number
 ): boolean {
-  if (this.passwordChangedAt) {
-    //getTime() is a Date function
-    //Assuming timestamp is given in seconds
-    // const changedTimeStamp = (this.passwordChangedAt as Date).getTime() / 1000; // for jwt timestamp
-    return this.passwordChangedAt > timestamp;
-  }
-  return false;
+	if (this.passwordChangedAt) {
+		//getTime() is a Date function
+		//Assuming timestamp is given in seconds
+		// const changedTimeStamp = (this.passwordChangedAt as Date).getTime() / 1000; // for jwt timestamp
+		return this.passwordChangedAt > timestamp;
+	}
+	return false;
 };
 
 userSchema.methods.createPasswordResetToken = function(this: IUser): string {
-  const resetToken = crypto.randomBytes(32).toString("hex");
-  // crypto module that doesn't require much processor
-  // Only use bcrypt for passwords
+	const resetToken = crypto.randomBytes(32).toString("hex");
+	// crypto module that doesn't require much processor
+	// Only use bcrypt for passwords
 
-  this.passwordResetToken = crypto
-    .createHash("sha256")
-    .update(resetToken)
-    .digest("hex");
+	this.passwordResetToken = crypto
+		.createHash("sha256")
+		.update(resetToken)
+		.digest("hex");
 
-  this.passwordResetExpires = Date.now() + 10 * 60 * 1000; // 10 minutes
-  return resetToken; // returns unhashed token
+	this.passwordResetExpires = Date.now() + 10 * 60 * 1000; // 10 minutes
+	return resetToken; // returns unhashed token
 };
 
 export const Users = mongoose.model<IUser>("Users", userSchema);
